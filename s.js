355c2a9bf6a018b53c4b177dfe1c05d8b5a4a404
Shan23s4 <165127999+Shan23s4@users.noreply.github.com>
2024-03-27 12:21:01 +0500

    Add files via upload

---
+++ s.js
@@ -0,0 +1,9 @@
+let open=document.querySelector(".open")
+let nav=document.querySelector(".nav")
+let cato=document.querySelector(".cato")
+open.addEventListener("click",()=>{
+    nav.classList.toggle("op")
+  
+
+    console.log(nav)
+})
